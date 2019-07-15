9a61bbc009a62a65b04f5e14f561ca93c2cf0221
Greg Duckworth <gregoryduckworth@googlemail.com>
2019-07-15 17:32:25 +0100

    Include version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,92 +21,92 @@
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body class="h-100">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        @auth
-                        <li class="nav-link">
-                            <a href="{{ route('leagues.index') }}">Leagues</a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="{{ route('teams.index') }}">Teams</a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="{{ route('players.index') }}">Players</a>
-                        </li>
-                        @endauth
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    @auth
+                    <li class="nav-link">
+                        <a href="{{ route('leagues.index') }}">Leagues</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="{{ route('teams.index') }}">Teams</a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="{{ route('players.index') }}">Players</a>
+                    </li>
+                    @endauth
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @auth
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endauth
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endauth
+    <section class="container flex-grow py-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                @if(session()->get('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+                @endif
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+                @endif
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
                     </ul>
-                </div>
+                </div><br />
+                @endif
+                @yield('content')
             </div>
-        </nav>
+        </div>
+    </section>
 
-        <main class="py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-12">
-                        @if(session()->get('success'))
-                        <div class="alert alert-success">
-                            {{ session()->get('success') }}
-                        </div>
-                        @endif
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                        @endif
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div><br />
-                        @endif
-                        @yield('content')
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
+    <footer class="container fixed-bottom">
+        <div class="float-right">{{ env('SOURCE_VERSION') ?? "No commit version" }}</div>
+    </footer>
 </body>
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
--- resources/views/leagues/show.blade.php
+++ resources/views/leagues/show.blade.php
@@ -1,124 +1,124 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
-    <div class="card-header">
-        {{ $league->name }}
-        <a href="{{ url()->previous() }}" class="pull-right">Back</a>
-    </div>
-    <div class="card-body d-block">
-        <table class="table" id="league_table">
-            <thead>
-                <th>Team Name</th>
-                <th>P</th>
-                <th>W</th>
-                <th>D</th>
-                <th>L</th>
-                <th>GF</th>
-                <th>GA</th>
-                <th>+/-</th>
-                <th>Points</th>
-            </thead>
-            <tbody>
-                @foreach($league->teams as $team)
-                <tr>
-                    <td>
-                        <a href="{{ route('teams.show',$team->id) }}">{{ $team->name }}</a>
-                    </td>
-                    <td>
-                        {{ $team ->pivot->won + $team ->pivot->drawn + $team ->pivot->lost }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->won }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->drawn }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->lost }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->goalsFor }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->goalsAgainst }}
-                    </td>
-                    <td>
-                        {{ ($team ->pivot->goalsFor - $team->pivot->goalsAgainst) }}
-                    </td>
-                    <td>
-                        {{ $team ->pivot->points }}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <hr>
-        <table class="table" id="fixture_table">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Home</th>
-                    <th></th>
-                    <th>v</th>
-                    <th></th>
-                    <th>Away</th>
-                    @auth
-                    <th>Actions</th>
-                    @endauth
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($league->fixtures as $fixture)
-                <tr>
-                    <td>{{ $fixture->date }}</td>
-                    <td><a href="{{ route('teams.show', $fixture->team_1) }}">{{ App\Team::find($fixture->team_1)->name }}</a></td>
-                    <td>{{ $fixture->team_1_score }}</td>
-                    <td>v</td>
-                    <td>{{ $fixture->team_2_score }}</td>
-                    <td><a href="{{ route('teams.show', $fixture->team_2) }}">{{ App\Team::find($fixture->team_2)->name }}</a></td>
-                    @auth
-                    <td><a href="{{ route('fixtures.edit',$fixture->id) }}" class="btn btn-warning btn-sm">Edit</a></td>
-                    @endauth
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <hr>
-        <table class="table" id="league_scorers">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Goals</th>
-                    <th>Team</th>
-                    @auth
-                    <th>Actions</th>
-                    @endauth
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($league->teams as $team)
-                    @foreach($team->players as $player)
+    <div class="card">
+        <div class="card-header">
+            {{ $league->name }}
+            <a href="{{ url()->previous() }}" class="pull-right">Back</a>
+        </div>
+        <div class="card-body d-block">
+            <table class="table" id="league_table">
+                <thead>
+                    <th>Team Name</th>
+                    <th>P</th>
+                    <th>W</th>
+                    <th>D</th>
+                    <th>L</th>
+                    <th>GF</th>
+                    <th>GA</th>
+                    <th>+/-</th>
+                    <th>Points</th>
+                </thead>
+                <tbody>
+                    @foreach($league->teams as $team)
                     <tr>
-                        <td><a href="{{ route('players.show', $player->id) }}">{{ $player->name }}</a></td>
-                        <td>{{ $player->leagueGoals($league->id) }}</td>
-                        <td>{{ $team->name }}</td>
-                        @auth
                         <td>
-                            <a href="{{ route('players.editGoals', [$player->id, $team->id, $league->id]) }}"
-                                class="btn btn-warning btn-sm">
-                                Edit
-                            </a>
+                            <a href="{{ route('teams.show',$team->id) }}">{{ $team->name }}</a>
                         </td>
+                        <td>
+                            {{ $team ->pivot->won + $team ->pivot->drawn + $team ->pivot->lost }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->won }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->drawn }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->lost }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->goalsFor }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->goalsAgainst }}
+                        </td>
+                        <td>
+                            {{ ($team ->pivot->goalsFor - $team->pivot->goalsAgainst) }}
+                        </td>
+                        <td>
+                            {{ $team ->pivot->points }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <hr>
+            <table class="table" id="fixture_table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Home</th>
+                        <th></th>
+                        <th>v</th>
+                        <th></th>
+                        <th>Away</th>
+                        @auth
+                        <th>Actions</th>
+                        @endauth
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($league->fixtures as $fixture)
+                    <tr>
+                        <td>{{ $fixture->date }}</td>
+                        <td><a href="{{ route('teams.show', $fixture->team_1) }}">{{ App\Team::find($fixture->team_1)->name }}</a></td>
+                        <td>{{ $fixture->team_1_score }}</td>
+                        <td>v</td>
+                        <td>{{ $fixture->team_2_score }}</td>
+                        <td><a href="{{ route('teams.show', $fixture->team_2) }}">{{ App\Team::find($fixture->team_2)->name }}</a></td>
+                        @auth
+                        <td><a href="{{ route('fixtures.edit',$fixture->id) }}" class="btn btn-warning btn-sm">Edit</a></td>
                         @endauth
                     </tr>
                     @endforeach
-                @endforeach
-            </tbody>
-        </table>
-        <hr>
+                </tbody>
+            </table>
+            <hr>
+            <table class="table" id="league_scorers">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Goals</th>
+                        <th>Team</th>
+                        @auth
+                        <th>Actions</th>
+                        @endauth
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($league->teams as $team)
+                        @foreach($team->players as $player)
+                        <tr>
+                            <td><a href="{{ route('players.show', $player->id) }}">{{ $player->name }}</a></td>
+                            <td>{{ $player->leagueGoals($league->id) }}</td>
+                            <td>{{ $team->name }}</td>
+                            @auth
+                            <td>
+                                <a href="{{ route('players.editGoals', [$player->id, $team->id, $league->id]) }}"
+                                    class="btn btn-warning btn-sm">
+                                    Edit
+                                </a>
+                            </td>
+                            @endauth
+                        </tr>
+                        @endforeach
+                    @endforeach
+                </tbody>
+            </table>
+            <hr>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('script')
